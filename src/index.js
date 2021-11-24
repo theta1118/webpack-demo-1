fdba7884d912483c9f9f5a1cea78330eebd0cbba
theta1118 <1311134412@qq.com>
2021-11-24 15:43:07 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import x from './x.js'
 import png from './assets/1.png'
 console.log(png)
 
-// const div = document.getElementById('app')
+const div = document.getElementById('app')
 
 // div.innerHTML = `
 // <img src="${png}">
